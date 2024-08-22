6220f98e7991360539695df8b2d9ba406bc69928
maria596r <maria596royer@outlook.com>
2024-08-22 11:28:32 +0530

    Create src_components_Receipt.js

---
+++ src_components_Receipt.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Receipt.css';
+
+function Receipt() {
+  return (
+    <div className="receipt">
+      <h3>Receipt</h3>
+      <p>Expense: Office Supplies</p>
+      <p>Amount: $100</p>
+      <p>Date: 2024-08-10</p>
+    </div>
+  );
+}
+
+export default Receipt;
